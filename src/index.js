83ec5485337e72f95f43e14713ed6d43c207fc94
Ivan Mattie <mattieivan@gmail.com>
2018-03-05 08:35:48 -0700

    Added simple docblocks for entry point

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import {SimpleSlider as SliderController} from "./SimpleSlider.js";
 
 var __slideController = new SliderController();
 
+/**
+ * @param {Object} data
+ * @param {HTMLElement} data.element
+ * @param {String} data.selector
+ * @param {Number=} data.delay Defaults to 5000
+ * @param {Boolean} data.paused Defaults to false 
+ */
 export var init = function(data) {
     return __slideController.init(data);
 };
